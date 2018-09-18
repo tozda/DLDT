2d0d953413357953f91a26f35d419e552b21c716
tozda <tozda@post.pl>
2018-09-18 16:17:28 +0200

    Update tozdatools.py

--- tozdatools.py
+++ tozdatools.py
@@ -1,3 +1,38 @@
-# My own tools
+import urllib.request
+from html.parser import HTMLParser
+import re
 
-def getURLsonly(htmlfile):
+extractedURLs = [] # variable for extracted urls
+
+# pobiera kod htmlowy dostepny pod wskazanym adresem webowym (URL)
+def gethtmlfromurl(url, proxy):
+    # jeżli masz wartoś proxy to zdefiniuj proxy typu http
+    if proxy:
+        urllib.request.ProxyHandler({"http": proxy})
+        
+    # pobierz kod html z wskazanego adresu
+    with urllib.request.urlopen(url) as response:
+        htmlcode = response.read()
+        return htmlcode # i go zwróc
+
+# pobiera odnośniki typu 'a'/'href' z kodu html
+def gethreffromhtml(htmlcode):
+    parser = MyHTMLParser() # ninicjalizacja objektu HTMLParser
+    parser.feed(str(htmlcode)) # sparsowanie kodu html
+    parser.close()
+    return extractedURLs
+# ============================================================================
+class MyHTMLParser(HTMLParser): # Parser html
+    """
+    Parser for getting URLs
+    Only 'a' tags with 'title' attribute considered
+    """
+    
+    # Only tag 'a'
+    def handle_starttag(self, tag, attrs):
+
+        if tag == 'a':
+            extractedURLs.append(attrs)
+
+        return extractedURLs
+
